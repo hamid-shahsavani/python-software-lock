d7e3b67b66375bbb8aa5efccef100089005754f2
sys113 <24222077+sys113@users.noreply.github.com>
2020-06-15 21:33:21 +0430

    Update project.py

--- project.py
+++ project.py
@@ -6,10 +6,10 @@ from requests import get , ConnectionError
 from hashlib import md5
 
 def clear_screen():
-    if name == 'nt':
-        system('cls')
-    else:
-        system('clear')
+	if name == 'nt':
+		system('cls')
+	else:
+		system('clear')
 
 if name == 'nt':
 	home = (str(Path.home()))
@@ -21,28 +21,30 @@ open(home + 'license', 'a').close()
 code = md5((hex(getnode()) + text).encode('utf-8')).hexdigest()
 license = md5(code.encode('utf-8')).hexdigest()
 
+# PROJECT SOURCE CODE ...
+
 def main():
 	print('hello world!')
 	input('')
 try:
-    get('https://www.google.com/')
-    if open(home + 'license','r').read() == license:
-        main()
-    else:
-        print('\n\t\t' + ' --- CODED BY SYS113 --- ' + '\n')
-        print("\t" + "Code : %s"%code)
-        print("\n\t\tuser : "+getuser())
-        password = input("\n\t\tpass : ")
-        if password == license:
-                file = open(home + 'license','w')
-                file.write(password)
-                file.close()
-                clear_screen()
-                main()
-        else:
-                clear_screen()
-                print('error , worng password ...')
+	get('https://www.google.com/')
+	if open(home + 'license','r').read() == license:
+		main()
+	else:
+		print('\n\t\t' + ' --- CODED BY SYS113 --- ' + '\n')
+		print("\t" + "Code : %s"%code)
+		print("\n\t\tuser : "+getuser())
+		password = input("\n\t\tpass : ")
+		if password == license:
+				file = open(home + 'license','w')
+				file.write(password)
+				file.close()
+				clear_screen()
+				main()
+		else:
+				clear_screen()
+				print('error , worng password ...')
 		input('')
 except ConnectionError:
-    print('error , connect to network ...')
-    input('')
+	print('error , connect to network ...')
+	input('')
